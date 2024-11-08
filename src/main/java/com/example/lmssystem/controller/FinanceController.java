8b14d5fdbd57752d272524098c3183970facb7ca
Olimova7060 <olimovab7060@gmail.com>
2024-11-08 17:20:35 +0500

    expencesDTO

--- src/main/java/com/example/lmssystem/controller/FinanceController.java
+++ src/main/java/com/example/lmssystem/controller/FinanceController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 @RestController
-@RequestMapping("/api/finance")
+@RequestMapping("/finance")
 public class FinanceController {
     private final FinanceService financeService;
 
