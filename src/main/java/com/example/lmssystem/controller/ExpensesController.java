f5335e25588f1315c72236734083442051ab0fb2
Marjona <lalisamarjona17@gmail.com>
2024-11-08 20:34:55 +0500

    profile

--- src/main/java/com/example/lmssystem/controller/ExpensesController.java
+++ src/main/java/com/example/lmssystem/controller/ExpensesController.java
@@ -12,17 +12,17 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/expenses")
-public class ExpencesController {
+public class ExpensesController {
 
     @Autowired
     private ExpencesService expencesService;
 
     @PostMapping
-    public ResponseEntity<?> createExpences(@RequestBody com.example.lmssystem.trnasfer.ExpencesDTO expencesDTO) {
+    public ResponseEntity<?> createExpenses(@RequestBody com.example.lmssystem.trnasfer.ExpencesDTO expencesDTO) {
         Expences newExpences = expencesService.saveExpences(expencesDTO);
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(ResponseData.builder()
-                        .message("Expence created successfully")
+                        .message("Expense created successfully")
                         .data(newExpences)
                         .success(true)
                         .build());
@@ -32,37 +32,37 @@ public class ExpencesController {
     public ResponseEntity<?> getAllExpences() {
         List<Expences> expencesList = expencesService.getAllExpences();
         return ResponseEntity.ok(ResponseData.builder()
-                .message("Expences retrieved successfully")
+                .message("Expenses retrieved successfully")
                 .data(expencesList)
                 .success(true)
                 .build());
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<?> getExpencesById(@PathVariable Long id) {
+    public ResponseEntity<?> getExpensesById(@PathVariable Long id) {
         Expences expences = expencesService.getExpencesById(id);
         return ResponseEntity.ok(ResponseData.builder()
-                .message("Expence found with id: " + id)
+                .message("Expense found with id: " + id)
                 .data(expences)
                 .success(true)
                 .build());
     }
 
 //    @PutMapping("/{id}")
-//    public ResponseEntity<?> updateExpences(@PathVariable Long id, @RequestBody  ExpencesDTO expencesDTO) {
-//        Expences updatedExpences = expencesService.updateExpences(id, expencesDTO);
+//    public ResponseEntity<?> updateExpenses(@PathVariable Long id, @RequestBody  ExpensesDTO expensesDTO) {
+//        Expenses updatedExpenses = expensesService.updateExpenses(id, expensesDTO);
 //        return ResponseEntity.ok(ResponseData.builder()
-//                .message("Expence updated successfully")
-//                .data(updatedExpences)
+//                .message("Expense updated successfully")
+//                .data(updatedExpenses)
 //                .success(true)
 //                .build());
 //    }
 
 //    @DeleteMapping("/{id}")
-//    public ResponseEntity<?> deleteExpences(@PathVariable Long id) {
-//        expencesService.deleteExpences(id);
+//    public ResponseEntity<?> deleteExpenses(@PathVariable Long id) {
+//        expensesService.deleteExpenses(id);
 //        return ResponseEntity.ok(ResponseData.builder()
-//                .message("Expence deleted successfully")
+//                .message("Expense deleted successfully")
 //                .success(true)
 //                .build());
 //    }
